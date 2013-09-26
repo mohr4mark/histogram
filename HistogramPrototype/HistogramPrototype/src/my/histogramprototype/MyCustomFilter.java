e040407f6c47651fde314c63fa4361f9b38a8fb3
SuperSneaks <supersneaks@gmail.com>
2013-09-26 11:49:36 -0400

    Create MyCustomFilter.java
    File filter for only display .txt files when the user is importing/opening files

---
+++ HistogramPrototype/HistogramPrototype/src/my/histogramprototype/MyCustomFilter.java
@@ -0,0 +1,27 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package my.histogram;
+
+import java.io.File;
+
+/**
+ *
+ * @author chthomson_it
+ */
+class MyCustomFilter extends javax.swing.filechooser.FileFilter {
+    
+    @Override
+        public boolean accept(File file) {
+            // Allow only directories, or files with ".txt" extension
+            return file.isDirectory() || file.getAbsolutePath().endsWith(".txt");
+        }
+    @Override
+        public String getDescription() {
+            // This description will be displayed in the dialog,
+            // hard-coded = ugly, should be done via I18N
+            return "Text documents (*.txt)";
+        }
+    
+}
